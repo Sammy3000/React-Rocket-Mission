2ca42e58c4390cf3916bb1a36fed5b6670fa6d65
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-03-06 22:23:33 +0300

    Solve merge conflicts

--- src/component/displayMission.js
+++ src/component/displayMission.js
@@ -8,12 +8,12 @@ const displayMission = () => (
     </div>
     <div className={styles.missionInfo}>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet
-        aliquet diam. Cras ullamcorper est quis odio efficitur suscipit. Quisque
-        vitae cursus dolor. Cras vitae justo velit. Phasellus venenatis iaculis
-        ornare. Etiam egestas egestas vulputate. Pellentesque nibh lorem, cursus
-        eget dolor ut, finibus fermentum est. Vivamus eu consequat elit,
-        tincidunt egestas diam. Donec tincidun
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit
+        amet aliquet diam. Cras ullamcorper est quis odio efficitur suscipit.
+        Quisque vitae cursus dolor. Cras vitae justo velit. Phasellus
+        venenatis iaculis ornare. Etiam egestas egestas vulputate.
+        Pellentesque nibh lorem, cursus eget dolor ut, finibus fermentum est.
+        Vivamus eu consequat elit, tincidunt egestas diam. Donec tincidun
       </p>
     </div>
     <div className={styles.buttons}>
