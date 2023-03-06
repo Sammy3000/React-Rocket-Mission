0a46bb66b473bd6080123d2aa302775923e5a982
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-03-06 21:02:42 +0300

    fix linter errors

--- src/component/displayMission.js
+++ src/component/displayMission.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../styles/DisplayMission.module.css';
+import styles from "../styles/DisplayMission.module.css"; // eslint-disable-line
 
 const displayMission = () => (
   <div className={styles.missionItems}>
@@ -8,12 +8,12 @@ const displayMission = () => (
     </div>
     <div className={styles.missionInfo}>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit
-        amet aliquet diam. Cras ullamcorper est quis odio efficitur suscipit.
-        Quisque vitae cursus dolor. Cras vitae justo velit. Phasellus
-        venenatis iaculis ornare. Etiam egestas egestas vulputate.
-        Pellentesque nibh lorem, cursus eget dolor ut, finibus fermentum est.
-        Vivamus eu consequat elit, tincidunt egestas diam. Donec tincidun
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet
+        aliquet diam. Cras ullamcorper est quis odio efficitur suscipit. Quisque
+        vitae cursus dolor. Cras vitae justo velit. Phasellus venenatis iaculis
+        ornare. Etiam egestas egestas vulputate. Pellentesque nibh lorem, cursus
+        eget dolor ut, finibus fermentum est. Vivamus eu consequat elit,
+        tincidunt egestas diam. Donec tincidun
       </p>
     </div>
     <div className={styles.buttons}>
