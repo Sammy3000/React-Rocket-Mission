4c61a923f5d0dd4e894a3c5934465b3efb1a65d2
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-03-06 20:37:05 +0300

    Configure store

---
+++ src/features/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import missionReducer from "./missions/missionSlice";
+
+const store = configureStore({
+  reducer: {
+    mission: missionReducer,
+  },
+});
+export default store;
