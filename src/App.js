8abbed9602f34eefc95566a7aa34959e2678ceed
Rachel Isaac <rachelisaac995@gmail.com>
2023-03-06 16:01:37 +0400

    Add routes to navlinks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Missions from './component/Missions';
+import MyProfile from './component/MyProfile';
+import Navbar from './component/Navbar';
+import NotFound from './component/NotFound';
+import Rockets from './component/Rockets';
 
 function App() {
   return (
     <div className="App">
-      <h1>OUR SPACE APP: LETS START HERE</h1>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={<MyProfile />}
+        />
+        <Route
+          path="/missions"
+          element={<Missions />}
+        />
+        <Route
+          path="/rockets"
+          element={<Rockets />}
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
     </div>
   );
 }
