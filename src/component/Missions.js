38c16e4568e1abf712242cbdc9b7e6b076ccc1f6
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-03-06 18:48:54 +0300

    Create missions header and style

--- src/component/Missions.js
+++ src/component/Missions.js
@@ -1,9 +1,22 @@
-import React from 'react';
-
+import React from "react";
+import displayMission from "./displayMission";
+import styles from "../styles/Missions.module.css";
 function Missions() {
   return (
-    <div>
-      <h1>Missions page</h1>
+    <div className={styles.missionsContainer}>
+      <div className={styles.missionsHeader}>
+        <div>
+          {" "}
+          <p>Mission</p>
+        </div>
+        <div>
+          <p>Description</p>
+        </div>
+        <div>
+          <p>Status</p>
+        </div>
+        <div />
+      </div>
     </div>
   );
 }
