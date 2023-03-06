69ce57470970fbf4dd5ea85bc7621ff108294072
Rachel Isaac <rachelisaac995@gmail.com>
2023-03-06 16:01:19 +0400

    Add components to tree

---
+++ src/component/MyProfile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MyProfile() {
+  return (
+    <div>
+      <h1>My profile Page</h1>
+    </div>
+  );
+}
+
+export default MyProfile;
---
+++ src/component/Rockets.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Rockets() {
+  return (
+    <div>
+      <h1>Rockets page</h1>
+    </div>
+  );
+}
+
+export default Rockets;
