9c18bfbb811fc9dd5380bfe9d0086089885e50af
Rachel Isaac <rachelisaac995@gmail.com>
2023-03-07 13:29:24 +0400

    Fetch data from API and dispatch action

--- src/features/rockets/rocketsSlice.js
+++ src/features/rockets/rocketsSlice.js
@@ -1,13 +1,41 @@
-import { createSlice } from '@reduxjs/toolkit';
+/* eslint-disable no-param-reassign */
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const url = 'https://api.spacexdata.com/v3/rockets';
+
+export const fetchRockets = createAsyncThunk(
+  'rockets/fetchRockets',
+  async () => {
+    const response = await axios.get(url);
+    return response.data;
+  },
+);
 
 const initialState = {
   rockets: [],
+  status: 'idle',
+  error: null,
 };
 
 const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRockets.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchRockets.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.rockets = action.payload;
+      })
+      .addCase(fetchRockets.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
 });
 
 export default rocketsSlice.reducer;
