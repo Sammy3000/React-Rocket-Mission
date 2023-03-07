353b5e47f4546f8e3f5865737edc46e14e6a9ee8
Rachel Isaac <rachelisaac995@gmail.com>
2023-03-07 11:14:27 +0400

    Edit displayMission project

--- src/component/displayMission.js
+++ src/component/displayMission.js
@@ -8,21 +8,27 @@ const displayMission = () => (
     </div>
     <div className={styles.missionInfo}>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit
-        amet aliquet diam. Cras ullamcorper est quis odio efficitur suscipit.
-        Quisque vitae cursus dolor. Cras vitae justo velit. Phasellus
-        venenatis iaculis ornare. Etiam egestas egestas vulputate.
-        Pellentesque nibh lorem, cursus eget dolor ut, finibus fermentum est.
-        Vivamus eu consequat elit, tincidunt egestas diam. Donec tincidun
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet
+        aliquet diam. Cras ullamcorper est quis odio efficitur suscipit. Quisque
+        vitae cursus dolor. Cras vitae justo velit. Phasellus venenatis iaculis
+        ornare. Etiam egestas egestas vulputate. Pellentesque nibh lorem, cursus
+        eget dolor ut, finibus fermentum est. Vivamus eu consequat elit,
+        tincidunt egestas diam. Donec tincidun
       </p>
     </div>
     <div className={styles.buttons}>
-      <button type="button" className={styles.memberBtn}>
+      <button
+        type="button"
+        className={styles.memberBtn}
+      >
         NOT A MEMBER
       </button>
     </div>
     <div className={styles.buttons}>
-      <button type="button" className={styles.joinMissionBtn}>
+      <button
+        type="button"
+        className={styles.joinMissionBtn}
+      >
         Join Mission
       </button>
     </div>
