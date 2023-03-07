40d2176f56bd6ef3a8492fcdd76905bc54aaa763
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-03-07 12:30:47 +0300

    Add pending and rejected status in extrareducers

--- src/component/Missions.js
+++ src/component/Missions.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from "react";
-import DisplayMission from "./DisplayMission";
-import styles from "../styles/Missions.module.css";
-import { useDispatch, useSelector } from "react-redux";
-import { getMission } from "../features/missions/missionSlice";
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import DisplayMission from './DisplayMission';
+import styles from '../styles/Missions.module.css';
+import { getMission } from '../features/missions/missionSlice';
+
 function Missions() {
   const dispatch = useDispatch();
-  const { mission } = useSelector((store) => store.mission);
   useEffect(() => {
     dispatch(getMission());
   }, [dispatch]);
@@ -23,16 +23,7 @@ function Missions() {
         </div>
         <div />
       </div>
-      {mission.map((mision) => {
-        return (
-          <DisplayMission
-            key={mision.mission_id}
-            id={mision.mission_id}
-            name={mision.mission_name}
-            description={mision.description}
-          />
-        );
-      })}
+      <DisplayMission />
     </div>
   );
 }
--- src/features/missions/missionSlice.js
+++ src/features/missions/missionSlice.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-param-reassign */
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-//Get missions from API
+// Get missions from API
 export const getMission = createAsyncThunk("mission/getMission", async () => {
   const result = await axios("https://api.spacexdata.com/v3/missions");
   return result.data;
@@ -8,6 +9,8 @@ export const getMission = createAsyncThunk("mission/getMission", async () => {
 
 const initialState = {
   mission: [],
+  status: "idle",
+  error: null,
 };
 
 const missionSlice = createSlice({
@@ -15,9 +18,18 @@ const missionSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(getMission.fulfilled, (state, action) => {
-      state.mission = action.payload;
-    });
+    builder
+      .addCase(getMission.fulfilled, (state, action) => {
+        state.mission = action.payload;
+        state.status = "succeded";
+      })
+      .addCase(getMission.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(getMission.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
   },
 });
 export default missionSlice.reducer;
