951d9665a71cd634121e322b9cdc13a687cdda4d
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-03-07 12:33:20 +0300

    Fix linter errors

--- src/features/missions/missionSlice.js
+++ src/features/missions/missionSlice.js
@@ -1,33 +1,33 @@
 /* eslint-disable no-param-reassign */
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 // Get missions from API
-export const getMission = createAsyncThunk("mission/getMission", async () => {
-  const result = await axios("https://api.spacexdata.com/v3/missions");
+export const getMission = createAsyncThunk('mission/getMission', async () => {
+  const result = await axios('https://api.spacexdata.com/v3/missions');
   return result.data;
 });
 
 const initialState = {
   mission: [],
-  status: "idle",
+  status: 'idle',
   error: null,
 };
 
 const missionSlice = createSlice({
-  name: "mission",
+  name: 'mission',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(getMission.fulfilled, (state, action) => {
         state.mission = action.payload;
-        state.status = "succeded";
+        state.status = 'succeded';
       })
-      .addCase(getMission.pending, (state, action) => {
-        state.status = "loading";
+      .addCase(getMission.pending, (state) => {
+        state.status = 'loading';
       })
       .addCase(getMission.rejected, (state, action) => {
-        state.status = "failed";
+        state.status = 'failed';
         state.error = action.error.message;
       });
   },
