425412c737bcacee0cd2a3942d5fa9f1f4a64c40
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-03-07 21:25:58 +0300

    Fix linter errors

--- src/features/missions/missionSlice.js
+++ src/features/missions/missionSlice.js
@@ -1,44 +1,44 @@
 /* eslint-disable no-param-reassign */
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 // Get missions from API
-export const getMission = createAsyncThunk("mission/getMission", async () => {
-  const result = await axios("https://api.spacexdata.com/v3/missions");
+export const getMission = createAsyncThunk('mission/getMission', async () => {
+  const result = await axios('https://api.spacexdata.com/v3/missions');
   return result.data;
 });
 
 const initialState = {
   mission: [],
-  status: "idle",
+  status: 'idle',
   error: null,
 };
 
 const missionSlice = createSlice({
-  name: "mission",
+  name: 'mission',
   initialState,
   reducers: {
     joinMission: (state, action) => {
       const missions = state.mission.find(
-        (result) => result.mission_id === action.payload
+        (result) => result.mission_id === action.payload,
       );
-      missions.reserved = !missions.reserved;
+      missions.reserved = true;
     },
   },
   extraReducers: (builder) => {
     builder
       .addCase(getMission.fulfilled, (state, action) => {
         state.mission = action.payload;
-        state.status = "succeded";
+        state.status = 'succeded';
       })
       .addCase(getMission.pending, (state) => {
-        state.status = "loading";
+        state.status = 'loading';
       })
       .addCase(getMission.rejected, (state, action) => {
-        state.status = "failed";
+        state.status = 'failed';
         state.error = action.error.message;
       });
   },
 });
-export const getReservedMissions = (state) =>
-  state.missions.missions.filter((results) => results.reserved);
+const reservedMissions = (state) => state.missions.missions.filter((results) => results.reserved);
+export { reservedMissions };
 export default missionSlice.reducer;
