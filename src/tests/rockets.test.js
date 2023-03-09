281d4322507f084073257fb6b518e13cd10dbe3d
Rachel Isaac <rachelisaac995@gmail.com>
2023-03-09 14:03:00 +0400

    Add tests for rocket component

---
+++ src/tests/rockets.test.js
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import Rockets from '../component/Rockets';
+import store from '../features/store';
+
+describe('testing snapshots of Rocket component', () => {
+  it('testing Rockets component', () => {
+    const rocketComponent = render(
+      <Provider store={store}>
+        <Rockets />
+      </Provider>
+    );
+    expect(rocketComponent).toMatchSnapshot();
+  });
+});
